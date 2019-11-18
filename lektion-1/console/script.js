de4a7ef0c1eb906e51c65300fc989a85dfab6cc8
Geylan Ghafur <ripole-96@hotmail.com>
2019-11-18 13:46:50 +0100

    lektion 1

---
+++ lektion-1/console/script.js
@@ -0,0 +1,39 @@
+// en kommentar kan skrivas så här
+/*   en kommentar kan skrivas så här */
+
+/* märkspråk             scriptspråk                    programmeringspråk
+HTML                       Javascript                       C  
+CSS                        Powershell                       C++
+XML                        cmdLets                          C#  
+                           (Python)                         Java
+                                                            (Python)
+
+JAVA ÄR INTE JAVASCRIPT
+
+Högnivå språk (mellan användaren och systemet)
+---------------------------------------------
+ (Javascript)
+ Python
+ C# / Java
+ Delphi
+
+
+ Lågnivå språk (mellan systemet och hårdvara)
+ -----------------------------------------------
+ C++
+ C
+ Cobol
+ Ansembler
+ Mikrokod
+*/
+
+// skriver ut på själva hemsidan
+document.body.innerText = 'Detta är en text ';
+
+// skriver ut i konsolen
+console.log ('Här vi ut nått i konsolen');
+console.warn ('Detta är en varning!');
+console.error ('Detta är ett felmeddelande!');
+
+// SANDBOX - DEVELOPER - Här loggar vi allt!
+// PRODUCTION - SKARPT LÄGE - Här loggar vi väldigt lite!
